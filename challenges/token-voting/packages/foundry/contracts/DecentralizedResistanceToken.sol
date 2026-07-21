10bd8f34555c2593dc27d389a6e9b78ee0986730
ssghost <ssprof0@gmail.com>
2026-07-21 18:11:22 +0800

    remove lib directory from tracking

--- challenges/token-voting/packages/foundry/contracts/DecentralizedResistanceToken.sol
+++ challenges/token-voting/packages/foundry/contracts/DecentralizedResistanceToken.sol
@@ -1,26 +1,90 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
-import { console2 } from "../lib/forge-std/src/console2.sol";
-import { Voting } from "./Voting.sol";
+interface IVoting {
+    function removeVotes(address from) external;
+}
 
-contract DecentralizedResistanceToken is ERC20, Ownable(msg.sender) {
+contract DecentralizedResistanceToken {
+    string public constant name = "Decentralized Resistance Token";
+    string public constant symbol = "DRT";
+    uint8 public constant decimals = 18;
+
+    uint256 public totalSupply;
+    address public owner;
     address public votingContract;
 
-    constructor(uint256 initialSupply) ERC20("DecentralizedResistanceToken", "DRT") {
-        _mint(msg.sender, initialSupply);
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    error NotOwner();
+    error ZeroAddress();
+    error InsufficientBalance();
+    error InsufficientAllowance();
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    modifier onlyOwner() {
+        if (msg.sender != owner) revert NotOwner();
+        _;
     }
 
-    function setVotingContract(address _votingContract) external onlyOwner {
-        votingContract = _votingContract;
+    constructor(uint256 initialSupply) {
+        owner = msg.sender;
+        emit OwnershipTransferred(address(0), msg.sender);
+
+        totalSupply = initialSupply;
+        balanceOf[msg.sender] = initialSupply;
+
+        emit Transfer(address(0), msg.sender, initialSupply);
     }
 
-    function _update(address from, address to, uint256 amount) internal override {
-        if (votingContract != address(0) && from != address(0)) {
-            Voting(votingContract).removeVotes(from);
+    function setVotingContract(address _voting) external onlyOwner {
+        votingContract = _voting;
+    }
+
+    function transfer(address to, uint256 amount) external returns (bool) {
+        _transfer(msg.sender, to, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 amount) external returns (bool) {
+        uint256 currentAllowance = allowance[from][msg.sender];
+        if (currentAllowance < amount) revert InsufficientAllowance();
+
+        unchecked {
+            allowance[from][msg.sender] = currentAllowance - amount;
         }
-        super._update(from, to, amount);
+
+        emit Approval(from, msg.sender, allowance[from][msg.sender]);
+
+        _transfer(from, to, amount);
+        return true;
+    }
+
+    function _transfer(address from, address to, uint256 amount) internal {
+        if (to == address(0)) revert ZeroAddress();
+
+        uint256 fromBalance = balanceOf[from];
+        if (fromBalance < amount) revert InsufficientBalance();
+
+        if (votingContract != address(0)) {
+            IVoting(votingContract).removeVotes(from);
+        }
+
+        unchecked {
+            balanceOf[from] = fromBalance - amount;
+            balanceOf[to] += amount;
+        }
+
+        emit Transfer(from, to, amount);
     }
 }
